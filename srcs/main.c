9adaef4e9eef11c185b038af89e9bbe87c86ad44
gundz <fgundlac@student.42.fr>
2014-10-03 19:03:41 +0200

    solved option bug

--- srcs/main.c
+++ srcs/main.c
@@ -2,8 +2,6 @@
 #include <ls.h>
 #include <stddef.h>
 
-#include <stdio.h>
-
 void				main_ls(t_list **lst, char **argv, int all)
 {
 	int				i;
@@ -28,12 +26,11 @@ int					main(int argc, char **argv)
 	t_list			*lst;
 
 	nb_options = options(argc, argv, tab);
-	printf("%d\n", nb_options);
 	lst = NULL;
-	if (argc > 1)
-		main_ls(&lst, argv, get_option(tab, 'a'));
-	if (argc > 1 && nb_options == 0)
+	if (nb_options == (argc - 1))
 		lst_push_back(&lst, store_dir_content("./", get_option(tab, 'a')));
+	else 
+		main_ls(&lst, argv, get_option(tab, 'a'));
 	sort_all(&lst, get_option(tab, 'r'));
 	show_dir(lst);
 	return (0);
